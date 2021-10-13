0b846fa27d53af8e143bedbc605e9bad5388c168
ak1909552 <82260503+ak1909552@users.noreply.github.com>
2021-10-13 22:15:00 +0300

    Create m_color.java

---
+++ backtracking/src/backtracking/m_color.java
@@ -0,0 +1,46 @@
+package backtracking;
+
+public class m_color {
+	//Given and undirected graph and a number m, determine if the graph can be coloured with at most m colours such that no two adjacent vertices of the graph are colored with the same color.
+	public boolean find_color(int[][] matrix, int[] colors, int max_colors, int node) {
+		   //we have to backwards compare in backtracking
+		   if(node == matrix.length -1) {
+		       return true;
+		   }
+		   //we have completed the base case
+		   // now we need to check through the adjacency matrix
+		   // first we must assign a color
+		   for(int i = 1; i <= max_colors; i++) {
+		       // call is valid and set the color[node] if true
+		       if(isValid(matrix, i, node, colors))
+		           colors[node] = i;
+		       // then call find_color again and check if its true
+		       if(find_color(matrix, colors, max_colors, node)) {
+		           return true;
+		       }
+		       // if not then color[node] = 0;
+		       colors[node] = 0;
+		   }
+		   
+		   return false;
+		   // we return false when there is no possible solution
+		   
+		       
+		}
+		public boolean isValid(int[][] matrix, int color, int node, int[] colors) {
+		   //returns true if this node can accept this color?
+		   //we have to check if the node can accept this color
+		   for(int i = 0; i < matrix.length; i++) {
+		       //check if the color of the adjacent is not equal to this color
+		       if(matrix[node][i] == 1) {
+		           //this means that the node shares an edge with the current node
+		           if(colors[node] == color) {
+		               //adjacent node has the same color then return false
+		               return false;
+		           }
+		       }
+		   }
+		   return true;
+		}
+	
+}
