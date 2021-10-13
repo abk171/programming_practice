3e9d7e499c1c6d1e2e0102ecffc53e18ce3b11fc
ak1909552 <82260503+ak1909552@users.noreply.github.com>
2021-10-13 22:35:43 +0300

    Update m_color.java

--- backtracking/src/backtracking/m_color.java
+++ backtracking/src/backtracking/m_color.java
@@ -1,5 +1,7 @@
 package backtracking;
 
+import java.util.Arrays;
+
 public class m_color {
 	//Given and undirected graph and a number m, determine if the graph can be coloured with at most m colours such that no two adjacent vertices of the graph are colored with the same color.
 	public boolean find_color(int[][] matrix, int[] colors, int max_colors, int node) {
@@ -12,18 +14,22 @@ public class m_color {
 		   // first we must assign a color
 		   for(int i = 1; i <= max_colors; i++) {
 		       // call is valid and set the color[node] if true
-		       if(isValid(matrix, i, node, colors))
-		           colors[node] = i;
-		       // then call find_color again and check if its true
-		       if(find_color(matrix, colors, max_colors, node)) {
-		           return true;
+		       if(isValid(matrix, i, node, colors)) {
+		    	   colors[node] = i;
+			       // then call find_color again and check if its true
+			       if(find_color(matrix, colors, max_colors, node+1)) {
+			           return true;
+			       }
+			       // if not then color[node] = 0;
+			       colors[node] = 0;
 		       }
-		       // if not then color[node] = 0;
-		       colors[node] = 0;
+		      
+		       
 		   }
 		   
 		   return false;
 		   // we return false when there is no possible solution
+		   
 		   
 		       
 		}
@@ -34,7 +40,7 @@ public class m_color {
 		       //check if the color of the adjacent is not equal to this color
 		       if(matrix[node][i] == 1) {
 		           //this means that the node shares an edge with the current node
-		           if(colors[node] == color) {
+		           if(colors[i] == color) {
 		               //adjacent node has the same color then return false
 		               return false;
 		           }
@@ -42,5 +48,22 @@ public class m_color {
 		   }
 		   return true;
 		}
+		
+		
+		
+		public static void main(String args[]) {
+			   int graph[][] = {
+			            { 0, 1, 1, 1 },
+			            { 1, 0, 1, 0 },
+			            { 1, 1, 0, 1 },
+			            { 1, 0, 1, 0 },
+			        };
+			   int m = 3; // Number of colors
+			   int[] colors = new int[4];
+			   m_color c = new m_color();
+			   c.find_color(graph, colors, m, 0);
+			  System.out.println( Arrays.toString(colors));
+		}
+		   
 	
 }
